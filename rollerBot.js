c1c111f8f28885ffb79c284630a712f4cb7f2be3
Zach Wagner <Zach Wagner>
2017-02-09 16:11:56 -0500

    1+ added to all d rolls

--- rollerBot.js
+++ rollerBot.js
@@ -10,29 +10,30 @@ rollerBot.on('reader', message => {
   message.channel.sendMessage('Ready to roll!');
 });
 
+//The user specifies what dice they'd like rolled and rollerBot makes a random number!
 rollerBot.on('message', message => {
     if(message.content === 'd20'){
-     diceResult = Math.floor(Math.random() * (20+1));
+     diceResult = 1+Math.floor(Math.random() * (20+1)); //The +1 occurs  because standard dice do not have 0's on them
       message.channel.sendMessage('You rolled a ' + diceResult);
     }
     else if(message.content === 'd12'){
-      diceResult = Math.floor(Math.random() * (12+1));
+      diceResult = 1+Math.floor(Math.random() * (12+1));
        message.channel.sendMessage('You rolled a ' + diceResult);
     }
     else if(message.content === 'd10'){
-      diceResult = Math.floor(Math.random() * (10+1));
+      diceResult = Math.floor(Math.random() * (10+1)); //D10 is unique as dice sets use 0 in order to do percentages
        message.channel.sendMessage('You rolled a ' + diceResult);
     }
     else if(message.content === 'd8'){
-      diceResult = Math.floor(Math.random() * (8+1));
+      diceResult = 1+Math.floor(Math.random() * (8+1));
        message.channel.sendMessage('You rolled a ' + diceResult);
     }
     else if(message.content === 'd6'){
-      diceResult = Math.floor(Math.random() * (6+1));
+      diceResult = 1+Math.floor(Math.random() * (6+1));
        message.channel.sendMessage('You rolled a ' + diceResult);
     }
     else if(message.content === 'd4'){
-      diceResult = Math.floor(Math.random() * (4+1));
+      diceResult = 1+Math.floor(Math.random() * (4+1));
        message.channel.sendMessage('You rolled a ' + diceResult);
     }
 });
